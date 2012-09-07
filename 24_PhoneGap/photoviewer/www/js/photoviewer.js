5e312115528348ea30f9d1e440c9549cf4356119
Takashi Okamoto <toraneko@gmail.com>
2012-09-07 23:37:59 +0900

    新しく追加した画像のタップホールドが効かないバグとPhotoSwipeの二重初期化を修正

--- 24_PhoneGap/photoviewer/www/js/photoviewer.js
+++ 24_PhoneGap/photoviewer/www/js/photoviewer.js
@@ -28,30 +28,30 @@ $(document).on('pagebeforecreate' ,'#p-gallery' ,function(e){
     }
   });
 }).on('pageshow' ,'#p-gallery' ,function(e){
-  // $.eachが終了した頃までタイマで待つ
-  // PhotoSwipeを初期化
-  iid = setInterval(function(){
-    if(domInitialized){
+  // PhotoSwipeが初期化されているかチェック(lengthが0なら未初期化)
+  if(Code.PhotoSwipe.instances.length==0){
+    iid = setInterval(function(){
       // イメージのリストの読み込みが終了したらPhotoSwipeの初期化を実行
-      var currentPage = $(e.target),
-      options = {cacheMode: Code.PhotoSwipe.Cache.Mode.normal},
-      photoSwipeInstance = $("ul.gallery a", e.target).photoSwipe(options,  "ps-"+currentPage.attr('id'));
-
-      // 画像一覧で画像を長押しされたらファイルを削除     
-      $("ul.gallery li a").on("taphold", function(e){
-        console.log("delete"+this);
-        e.preventDefault();
-        var src = $(this).attr("href");
-        src = src.substring(BASE_URL.length);
-        confirmDelete = confirm(src+"\nを削除しますか?");
-        if(confirmDelete){
-          $.get(BASE_URL + "delete.php?src="+src,function(e){});
-          $(this).remove();
-        }
-      });
-      clearInterval(iid);
-    }
-  },200);
+      if(domInitialized){
+        clearInterval(iid);
+        var currentPage = $(e.target),
+        options = {cacheMode: Code.PhotoSwipe.Cache.Mode.normal},
+        photoSwipeInstance = $("ul.gallery a", e.target).photoSwipe(options,  "ps-"+currentPage.attr('id'));
+        
+         // 画像一覧で画像を長押しされたらファイルを削除
+         $(document).on("taphold", "ul.gallery li a", function(e){
+          e.preventDefault();
+          var src = $(this).attr("href");
+          src = src.substring(BASE_URL.length);
+          confirmDelete = confirm(src+"\nを削除しますか?");
+          if(confirmDelete){
+            $.get(BASE_URL + "delete.php?src="+src,function(e){});
+            $(this).remove();
+          }
+        });
+      }
+    },2000);
+  }
 });
 
 $(document).on('pageinit', '#p-add-photo', function(e){
